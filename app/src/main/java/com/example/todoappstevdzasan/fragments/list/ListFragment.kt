28a94df401c0f274b7d8e3d5a15a634638247a31
Stefan <stefantosic.dev@gmail.com>
2021-03-04 13:49:36 +0100

    first commit

---
+++ app/src/main/java/com/example/todoappstevdzasan/fragments/list/ListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.todoappstevdzasan.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.todoappstevdzasan.R
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_list, container, false)
+
+        view.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        view.listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
+
+        return view
+    }
+}
